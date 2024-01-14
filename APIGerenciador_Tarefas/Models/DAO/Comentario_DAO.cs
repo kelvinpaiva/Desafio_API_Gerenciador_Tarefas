50bc2e9a6703dc805320e4bdb22a8231b6a41bd7
Kelvin de Oliveira Paiva <kelvinguir@hotmail.com>
2024-01-14 16:17:59 -0300

    Criação das classes DAO.

---
+++ APIGerenciador_Tarefas/Models/DAO/Comentario_DAO.cs
@@ -0,0 +1,10 @@
+﻿namespace APIGerenciador_Tarefas.Models.DAO
+{
+    public class Comentario_DAO
+    {
+        public int Id { get; set; }
+        public int IdTar { get; set; }
+        public string LctComentario { get; set; } = null!;
+        public int IdUsuario { get; set; }
+    }
+}
---
+++ APIGerenciador_Tarefas/Models/DAO/TarTarefa_DAO.cs
@@ -0,0 +1,14 @@
+﻿namespace APIGerenciador_Tarefas.Models.DAO
+{
+    public class TarTarefa_DAO
+    {
+        public int Id { get; set; }
+        public string TarTitulo { get; set; } = null!;
+        public string TarDescricao { get; set; } = null!;
+        public short TarPrioridade { get; set; }
+        public DateOnly TarDataValidade { get; set; }
+        public short TarStatus { get; set; }
+        public int IdPro { get; set; }
+        public int IdUsuario { get; set; }
+    }
+}
